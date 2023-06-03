72b313a42228f150612dc32d3b4492776b35292b
Iva Havranová <havranova.i@seznam.cz>
2023-06-03 12:58:01 +0200

    Joke component in progress

--- src/index.jsx
+++ src/index.jsx
@@ -2,55 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import { useState } from 'react';
+import { Joke } from './Joke';
 
 const App = () => {
-  const [likeUp, setLikeUp] = useState(0);
-  const [likeDown, setLikeDown] = useState(0);
-  const handleClickUp = () => {
-    setLikeUp(likeUp + 1);
-  };
-  const handleClickDown = () => {
-    setLikeDown(likeDown + 1);
-  };
-  return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={handleClickUp}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likeUp}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={handleClickDown}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {likeDown}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+  return <Joke />;
 };
 
 createRoot(document.querySelector('#app')).render(<App />);
