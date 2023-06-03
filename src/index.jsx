49de08bc97a2aaca17e5124901a7cd02d50157f6
Iva Havranová <havranova.i@seznam.cz>
2023-06-03 12:51:41 +0200

    done

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [likeUp, setLikeUp] = useState(0);
+  const [likeDown, setLikeDown] = useState(0);
+  const handleClickUp = () => {
+    setLikeUp(likeUp + 1);
+  };
+  const handleClickDown = () => {
+    setLikeDown(likeDown + 1);
+  };
   return (
-    <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
             <img
-              class="user-avatar"
+              className="user-avatar"
               src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
             />
-            <p class="user-name">Neroxx</p>
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up"></button>
-          <span id="likes-up" class="likes-count likes-count--up">
-            0
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={handleClickUp}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likeUp}
           </span>
-          <button id="btn-down" class="btn-like btn-like--down"></button>
-          <span id="likes-down" class="likes-count likes-count--down">
-            0
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={handleClickDown}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {likeDown}
           </span>
         </div>
       </div>
